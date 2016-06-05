9b85e4e5e11bb31edbed4002a998f9f387790f0c
Lucus16 <lars.jellema@gmail.com>
2016-06-05 22:45:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learn dvorak
+
+Just run the script, copy the lines exactly, when you're fast enough, you get
+the next letter.
+
+Wordlist is from https://github.com/first20hours/google-10000-english
---
+++ dvorak.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from random import choice
+from editdistance import eval as editdistance
+from time import time
+
+freq_order = 'etaoinshrdlucmfwypvbgkqjxz'
+dvorak_to_qwerty = dict()
+for d, q in zip("',.pyfgcrl/=aoeuidhtns-;qjkxbmwvz",
+        "qwertyuiop[]asdfghjkl;'zxcvbnm,./"):
+    dvorak_to_qwerty[d] = q
+
+with open('wordlist', 'r') as f:
+    all_words = [line.strip() for line in f.readlines()]
+
+def good_words(n):
+    alphabet = freq_order[:n + 1]
+    focus = freq_order[n]
+    def good(word):
+        if not all((c in alphabet for c in word)):
+            return False
+        if focus is not None and focus not in word:
+            return False
+        return True
+    return [word for word in all_words if good(word)]
+
+def genline(words):
+    if words == []: return ''
+    print(repr(words))
+    line = choice(words)
+    while True:
+        word = choice(words)
+        if len(word) + len(line) < 80:
+            line += ' ' + word
+        else:
+            break
+    return line
+
+def teachline(words):
+    line = genline(words)
+    print(line)
+    start = time()
+    entered = input()
+    end = time()
+    dtext = editdistance(line, entered)
+    dtime = int(end - start)
+    percentage = int(dtext / len(line))
+    print(str(dtext) + ' mistakes (' + str(percentage) + '%)')
+    print(str(dtime) + ' seconds')
+    return dtime, dtext
+
+def teach():
+    for i in range(len(freq_order)):
+        key = freq_order[i]
+        print('Next key: ' + key + ' (' + dvorak_to_qwerty[key] +
+            ' on qwerty keyboard)')
+        words = good_words(i)
+        dtime = 999999999
+        dtext = 999999999
+        while dtime > 100 or dtext > 1:
+            dtime, dtext = teachline(words)
+    while True:
+        teachline(all_words)
+
+if __name__ == '__main__':
+    try:
+        teach()
+    except EOFError:
+        pass
